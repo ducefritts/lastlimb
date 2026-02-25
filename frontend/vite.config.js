748a781943af46740b25db26cc5fcaab631ef25d
ducefritts <ducefri@gmail.com>
2026-02-25 14:05:25 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
   },
-  publicDir: 'public',
+  publicDir: 'public/assets',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist'
   }
 })
